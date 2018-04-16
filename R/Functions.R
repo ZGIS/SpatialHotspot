beddb4e58637a7efccaf784e2ae3d89975342895
Clemens Havas <chavas.its-m2014@fh-salzburg.ac.at>
2018-04-16 14:22:28 +0200

    fixed man file

--- R/Functions.R
+++ R/Functions.R
@@ -9,9 +9,7 @@ read.data <- function(fileName) {
 #' Calculate local Moran's I
 #' @export 
 moranI <- function(fileName) {
-  return(spdep::localmoran(fileName, listw, zero.policy=NULL, na.action=na.fail, 
-                           alternative = "greater", p.adjust.method="none", mlvar=TRUE,
-                           spChk=NULL))
+  #return(spdep::localmoran(fileName, listw, zero.policy=NULL, na.action=na.fail, alternative = "greater", p.adjust.method="none", mlvar=TRUE, spChk=NULL))
 }
 
 #'Perform Getis-Ord
