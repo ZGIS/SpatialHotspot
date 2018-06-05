dff52d7424166d082defed4096b85a16aa8fb81b
helenabergstedt <helena.bergstedt@sbg.ac.at>
2018-06-05 11:57:56 +0200

    tweets_new was wrong

--- R/moransi.R
+++ R/moransi.R
@@ -5,7 +5,7 @@
 #' @export 
 moranI <- function(tweets) {
   
-  nb <- knn2nb(knearneigh(tweets_new))
+  nb <- knn2nb(knearneigh(tweets))
   listw <- nb2listw(nb)
   
   return(spdep::localmoran(as.numeric(as.vector(tweets$tweet_id)), listw, zero.policy=NULL, alternative = "greater", p.adjust.method="none", mlvar=TRUE, spChk=NULL))
