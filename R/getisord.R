4264ecebee6c6475f07f0367a9249c760dabc178
Clemens Havas <chavas.its-m2014@fh-salzburg.ac.at>
2018-06-05 11:54:11 +0200

    spdev localG

--- R/getisord.R
+++ R/getisord.R
@@ -1,5 +1,7 @@
 #'Perform Getis-Ord
+#' @param x data to be analyzed
+#' @importFrom spdep localG 
 #' @export 
-getisOrd <- function() {
-  message("getis-ord comes here")
+getisOrd <- function(x) {
+  return(spdep::localG(x, listw, zero.policy=NULL, spChk=NULL, return_internals=FALSE, GeoDa=FALSE))
 }
